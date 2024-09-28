2c99929b47604a60e03ab3afa227df33613437e6
Aelkoulali <abdellah.elkoulali@gmx.fr>
2024-09-28 07:54:11 +0200

    Declare variables

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
+
+// Declare variables
 const menuToggle = document.querySelector('.menu-toggle');
 const navigation = document.querySelector('.navigation');
+
 menuToggle.onclick = () => {
   navigation.classList.toggle('open');
 }
